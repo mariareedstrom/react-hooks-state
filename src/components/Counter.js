70c4607f551cf8cb8768995f97de48f630936fca
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-01-14 12:13:47 -0600

    commit tree dd85d5dc3c91bd3a8d3baebcb4cef8da8e4c1cc1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,12 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    console.log(count);
+    setCount((currentCount) => currentCount + 1);
+    console.log(count);
+    setCount((currentCount) => currentCount + 1);
+    d;
+    console.log(count);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
